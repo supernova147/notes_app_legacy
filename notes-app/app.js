a7c54ea7b4bffbd801ded0f0d98d2f123d8858b9
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-08-29 16:34:58 -0400

    main content

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,32 +7,35 @@
 // fs.appendFileSync('notes.txt', ' byeee') //appending adds more text to the text file.
 
 // // const add = require('./utils.js');
-
 // // const sum = add(4, -2);
-
 // // console.log(sum);
+// const notes = require('./notes.js'); //So, we're bringing the function using require from notes.js
+// const getNotes = notes(); //delcaring a var to equal the function.
+// console.log(getNotes)// then we can call the function to the console log!
 
-
-// const notes = require('./notes.js');
-
-// console.log(notes);
 
 // // require is needed to load in core node modules
 // // we can also use require to load in other files as well
 // // export brings vars/functions or data in general you want to transfer from one file to the other 
 
-// const chalk = require('chalk')
-// const validator = require('validator')
+
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js') //loading in NPM package, yargs
 
 //Will note that I'm using ES6. Will also review legacy ways of using node.
-import chalk from 'chalk';
-import validator from 'validator';
+//^ no longer using ES6. USING LEGACY
+// import chalk from 'chalk';
+// import validator from 'validator';
 
-import getNotes from './notes.js';
+// import getNotes from './notes.js';
 
 // const msg = getNotes()
 
-// const greenMsg = chalk.green('Success!') //creating a var that utilizes chalk to change the color of a string.
+const greenMsg = chalk.green('Success!') //creating a var that utilizes chalk to change the color of a string.
+console.log(greenMsg); 
+
+// ^Using node app.js - The text appeared green & chalk was successful in changing the color.
 
 // console.log(msg) //console logging msg (runs getNotes functions which was retrieved from ./notes )
 
@@ -47,9 +50,91 @@ import getNotes from './notes.js';
 // I have been stuck on getting this message to turn green for a while..
 // I'll come back to it so I do not hinder my progression in the course.
 
-const command = process.argv[2]
+// const command = process.argv[2] 
+//grabbing the 2nd index of the array in argv;
 
-if (command === 'add') {
-    console.log('Adding node!')
-}
-console.log(process.argv[2])
+console.log(process.argv[2]) //argv[2] returns the value we want; which is the user's input in the terminal.
+//process = global variable; argv = argument vector - an array that contains all the arguments provided.
+
+// if (command === 'add') { // conditional function to capture whether the user
+//     console.log('Adding node!') // is trying to add a note 
+// } else if (command === 'remove') {
+//     console.log('Removing note') // or remove a note.
+// }
+
+//yargs - a package that assists with argument parsing.
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({ //Options object, where we can customize how it works
+    command: 'add', //name of command
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title', //describes what goes into title object
+            demandOption: true, //there must be an input for the title object.
+            type: 'string' //always have a string value for the title object
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) { //handler property that creates the action (function) that will be done when the command is called.
+        // console.log(`Title: ${argv.title}`); //Grabbing title value from the property in builder
+        // console.log(`Body: ${argv.body}`); 
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true, 
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse();
+
+// console.log(process.argv)
+// console.log(yargs.argv) // accessing the argv property on yargs
+// which lets yargs know to parse the arguments.
+
+
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,99 @@
+const fs = require('fs');
+const chalk = require('chalk')
 
-function getNotes() { //function created that we will be exporting
+const getNotes = () => { //function created that we will be exporting
     return ("Your notes...");
 }
 
+const addNote = (title, body) => { //function that will add notes
+    const notes = loadNotes() //loading the parsed data
+    // const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
+    // const duplicateNotes = notes.filter(function (note){
+    //     return note.title === title
+    //debugger/debugging notes*
+    //either console.log can be used to debug or debugger
+    debugger
+    //explicit error or no error messages are two errors that we may get when running node.js applications
+    // chrome://inspect/#devices - can expect node applications with chrome's inspect page
+    // 127.0.0.1:9229 -  was the port target used 
+    // ReferenceError - tells you whats wrong but not where exactly its wrong at
+    // 
 
+    if (!duplicateNote) { 
+        notes.push({ //using the push method to push the object & properties (title, body)
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken'))
+    }
+}
 
-module.exports = getNotes //Old way of exporting a function to a new file
+const saveNotes = (notes) => { //function that saves notes
+    const dataJSON = JSON.stringify(notes) //translate the data to a string 
+    fs.writeFileSync('notes.json', dataJSON) //writes the file to 'notes.json' with dataJSON's data
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json') //reading the notes from notes.json
+        const dataJSON = dataBuffer.toString() //turning the data to a string
+        return JSON.parse(dataJSON) //returning the parsed data
+    } catch (e) { //if an error is encountered, it will return an empty array aka error handling
+        return [];
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes() // var that calls a function that reads the file
+    const notesToKeep = notes.filter((note) => note.title !== title)
+    
+    // const notesToKeep = notes.filter(function (note) { //using filter method to find the notes we want to keep
+    //     return note.title !== title //returning all the notes that arent a match
+
+    if (notes.length > notesToKeep.length) { //conditional to check if notes are available to keep.
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else { // if there is no notes, then it will return no note found
+        console.log(chalk.red.inverse('No note found!'))
+    }
+    saveNotes(notesToKeep) //saving the new array
+}
+// module.exports = getNotes; //Old way of exporting a function to a new file
 // export default getNotes //I had to include "default" & the written command is slightly different.
 // Disregard the last comment, I'm approaching it the way Udemy has it; as close as possible.
+
+const listNotes = () => {
+    const notes = loadNotes()
+    
+    console.log(chalk.inverse('Your notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    }
+    else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
+
+
+module.exports = { //recreated the export as an object so multiple functions can be exported
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
